fca1ac0b394558b4a2d49fc26292a79759af55d1
Alan <ahuang7840@gmail.com>
2024-12-11 22:28:29 -0500

    fixed theme

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,12 +4,14 @@ import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "@/components/theme-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    return <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-    >
-        <SessionProvider>{children}</SessionProvider>
-    </ThemeProvider>;
+    return (
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem={false}
+            disableTransitionOnChange
+        >
+            <SessionProvider>{children}</SessionProvider>
+        </ThemeProvider>
+    );
 }
